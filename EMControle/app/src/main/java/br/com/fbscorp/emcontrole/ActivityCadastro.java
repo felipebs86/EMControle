c6d8ac2bcec25b9ef962e29cf2e5d5c987cd9aca
Felipe Bahiense <felipebahiensesantos@gmail.com>
2017-11-18 21:09:21 -0200

    Implementacao de botoes da tela de cadastro

--- EMControle/app/src/main/java/br/com/fbscorp/emcontrole/ActivityCadastro.java
+++ EMControle/app/src/main/java/br/com/fbscorp/emcontrole/ActivityCadastro.java
@@ -1,24 +1,44 @@
 package br.com.fbscorp.emcontrole;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.zip.Inflater;
 
 import br.com.fbscorp.emcontrole.helper.CadastroHelper;
 import br.com.fbscorp.emcontrole.model.Cadastro;
 
-public class ActivityCadastro extends AppCompatActivity {
+public class ActivityCadastro extends AppCompatActivity implements View.OnClickListener{
 
     private CadastroHelper helper;
+    private TextView txtData, txtHora;
+    private Button btnData;
+    private Button btnHora;
+    private Button btnImagem;
+    private int ano, mes, dia, hora, minuto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,23 @@ public class ActivityCadastro extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         helper = new CadastroHelper(this);
+
+        txtData = (TextView) findViewById(R.id.cad_txt_data);
+        txtHora = (TextView) findViewById(R.id.cad_txt_hora);
+        btnData = (Button) findViewById(R.id.cad_btn_data_inicio);
+        btnHora = (Button) findViewById(R.id.cad_btn_alarme);
+        btnImagem = (Button) findViewById(R.id.cad_btn_locais);
+
+        final Calendar cal = Calendar.getInstance();
+        ano = cal.get(Calendar.YEAR);
+        mes = cal.get(Calendar.MONTH);
+        dia = cal.get(Calendar.DAY_OF_MONTH);
+        hora = cal.get(Calendar.HOUR);
+        minuto = cal.get(Calendar.MINUTE);
+
+        btnData.setOnClickListener(this);
+        btnHora.setOnClickListener(this);
+        btnImagem.setOnClickListener(this);
 
     }
 
@@ -48,5 +85,67 @@ public class ActivityCadastro extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnData){
+            final Calendar c = Calendar.getInstance();
+            ano = c.get(Calendar.YEAR);
+            mes = c.get(Calendar.MONTH);
+            dia = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtData.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, ano, mes, dia);
+
+            datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+            datePickerDialog.show();
+        }
+
+        if (view == btnHora) {
+
+            final Calendar c = Calendar.getInstance();
+            hora = c.get(Calendar.HOUR_OF_DAY);
+            minuto = c.get(Calendar.MINUTE);
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtHora.setText(hourOfDay + ":" + minute);
+                        }
+                    }, hora, minuto, false);
+            timePickerDialog.show();
+        }
+
+        if(view == btnImagem){
+            AlertDialog.Builder ImageDialog = new AlertDialog.Builder(ActivityCadastro.this);
+            ImageDialog.setTitle("Locais de Aplicação");
+            ImageView showImage = new ImageView(ActivityCadastro.this);
+            ImageDialog.setView(showImage);
+
+            ImageDialog.setNegativeButton("ok", new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface arg0, int arg1)
+                {
+                }
+            });
+            ImageDialog.show();
+        }
+
     }
 }
